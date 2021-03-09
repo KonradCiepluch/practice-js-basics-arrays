a50c0cd72bc64850818a2e8adf4ca3bd1cd3d637
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-09 11:46:22 +0100

    Initialize let users

---
+++ 01/app.js
@@ -0,0 +1,4 @@
+let users = ['Tomasz Nowak', 'Bartosz Pawlak', 'Natalia Jagda', 'Szymon Boryna', 'Karolina Orłowska', 'Weronika Sosna'];
+
+console.log('Użytkownikiem pierwszym  jest: ', users[0],' Użytkownikiem trzecim jest: ', users[2],' Użytkownikiem piątym jest: ', users[4]);
+console.log(users.length);
