a3242b03ba7a2dadcf51bc732880b6d64444c8e6
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-09 13:05:06 +0100

    Add methods

--- 05/app.js
+++ 05/app.js
@@ -1,1 +1,10 @@
 const numbers = [1, 2, 3, 4, 5, 6, 7];
+
+const evenNumbers = numbers.filter(number => !(number%2));
+
+const sum = evenNumbers.reduce(function(a,b){
+    return a+b;
+})
+console.log(evenNumbers);
+console.log(sum);
+
