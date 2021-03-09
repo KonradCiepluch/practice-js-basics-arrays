d2d4197e18ee428e06fb9aa138f583cc92c4ccf5
Konrad Ciepluch <konrad190008@wp.pl>
2021-03-09 12:27:07 +0100

    Add for loop

--- 03/app.js
+++ 03/app.js
@@ -1,2 +1,11 @@
 const n = 24;
 const oddNumbers = [];
+
+for(let i=1; i<=n; i++){
+    if(i%2==1) {
+        oddNumbers.push(i);
+    }
+}
+for(let i=0; i<oddNumbers.length; i++){
+console.log(oddNumbers[i]);
+}
